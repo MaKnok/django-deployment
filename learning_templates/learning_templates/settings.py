945114156a5b5c1bf46e9d03d63874f726bfeb05
MaKnok <marinaknok@gmail.com>
2023-01-05 00:26:14 -0300

    host

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-2(g&!nun6qxbsjnhjritd@af#t(g5ph(*%uquhx*rz-zm&-_e3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  'maknok.pythonanywhere.com'
+]
 
 
 # Application definition
